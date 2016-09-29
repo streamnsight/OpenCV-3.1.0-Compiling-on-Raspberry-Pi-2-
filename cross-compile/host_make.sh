b5635e3f70708d0d93bbe90426c457748efff320
streamnsight <eleroy@msn.com>
2016-09-28 21:58:05 -0700

    use sshfs

--- cross-compile/host_make.sh
+++ cross-compile/host_make.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
+rm -rf build
+mkdir build
+pushd build
 
-# in same level as opencv-3.1.0
-
-cmake -D CMAKE_BUILD_TYPE=RELEASE \
--D CMAKE_INSTALL_PREFIX=../release \
+cmake -D CMAKE_TOOLCHAIN_FILE=../opencv-3.1.0/platforms/linux/arm-gnueabi.toolchain.cmake \
+-D CMAKE_BUILD_TYPE=RELEASE \
+-D CMAKE_CXX_COMPILER=/usr/bin/arm-linux-gnueabihf-g++ \
+-D CMAKE_C_COMPILER=/usr/bin/arm-linux-gnueabihf-gcc \
+-D CMAKE_INSTALL_PREFIX=/usr/local \
 -D INSTALL_C_EXAMPLES=OFF \
 -D INSTALL_PYTHON_EXAMPLES=ON \
 -D OPENCV_EXTRA_MODULES_PATH=../opencv_contrib-3.1.0/modules/ \
@@ -14,20 +18,21 @@ cmake -D CMAKE_BUILD_TYPE=RELEASE \
 -D WITH_NEON=ON \
 -D WITH_TBB=ON \
 -D BUILD_TBB=ON \
--D CMAKE_TOOLCHAIN_FILE=../opencv-3.1.0/platforms/linux/arm-gnueabi.toolchain.cmaketoolchain.cmake \
 -D CMAKE_C_FLAGS="-O3 -mfpu=neon -mfloat-abi=hard" \
 -D BUILD_JPEG=OFF \
--D JPEG_INCLUDE_DIR=../sysroot-chip/opt/libjpeg-turbo/include/ \
--D JPEG_LIBRARY=../sysroot-chip/opt/libjpeg-turbo/lib/libjpeg.a \
--D FFMPEG_INCLUDE_DIR=../sysroot-chip/usr/lib \
--D INCLUDE_DIRECTORIES=../sysroot-chip/usr/lib \
--D INCLUDE_DIRECTORIES=../sysroot-chip/usr/bin \
--D INCLUDE_DIRECTORIES=../sysroot-chip/opt/lib \
--D INCLUDE_DIRECTORIES=../sysroot-chip/lib \
--D PYTHON2_INCLUDE_PATH=../sysroot-chip/usr/include/python2.7 \
--D PYTHON2_LIBRARIES=../sysroot-chip/usr/lib/python2.7 \
--D PYTHON2_NUMPY_INCLUDE_DIRS=../sysroot-chip/usr/lib/python2.7/dist-packages \
--D PYTHON3_INCLUDE_PATH=../sysroot-chip/usr/include/python3.4 \
--D PYTHON3_LIBRARIES=../sysroot-chip/usr/lib/python3.4 \
--D PYTHON3_NUMPY_INCLUDE_DIRS=../sysroot-chip//usr/lib/python3.4/dist-packages \
+-D JPEG_INCLUDE_DIR=/mnt/chipfs/opt/libjpeg-turbo/include/ \
+-D JPEG_LIBRARY=/mnt/chipfs/opt/libjpeg-turbo/lib/libjpeg.a \
+-D FFMPEG_INCLUDE_DIR=/mnt/chipfs/usr/lib \
+-D INCLUDE_DIRECTORIES=/mnt/chipfs/usr/lib \
+-D INCLUDE_DIRECTORIES=/mnt/chipfs/usr/bin \
+-D INCLUDE_DIRECTORIES=/mnt/chipfs/opt/lib \
+-D INCLUDE_DIRECTORIES=/mnt/chipfs/lib \
+-D PYTHON2_INCLUDE_PATH=/mnt/chipfs/usr/include/python2.7 \
+-D PYTHON2_LIBRARIES=/mnt/chipfs/usr/lib/python2.7 \
+-D PYTHON2_NUMPY_INCLUDE_DIRS=/mnt/chipfs/usr/lib/python2.7/dist-packages \
+-D PYTHON3_INCLUDE_PATH=/mnt/chipfs/usr/include/python3.4 \
+-D PYTHON3_LIBRARIES=/mnt/chipfs/usr/lib/python3.4 \
+-D PYTHON3_NUMPY_INCLUDE_DIRS=/mnt/chipfs//usr/lib/python3.4/dist-packages \
 ../opencv-3.1.0/
+
+popd
